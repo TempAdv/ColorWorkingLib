30ec50e706033760dab63e8f2d7d2b74524ed9ca
TempAdv <TempAdv57@gmail.com>
2023-04-25 11:32:09 +0300

    multiply function
    multiply function overloaded

--- main.py
+++ main.py
@@ -31,6 +31,18 @@ class color:
             
         return color(R, G, B, 1)
 
+    def __mul__(self, OtherColor):
+
+        #multiplying colors, Alpha of color maters how much color channel will matter on result. Alpha of final color is 1
+        
+        A1 = self.A; A2 = OtherColor.A
+        
+        R = int(self.R * A1 * OtherColor.R * A2 / 255)
+        G = int(self.G * A1 * OtherColor.G * A2 / 255)
+        B = int(self.B * A1 * OtherColor.B * A2 / 255)
+            
+        return color(R, G, B, 1)
+
     
     def __str__(self):
         return f'{self.R}, {self.G}, {self.B}, {self.A}'
